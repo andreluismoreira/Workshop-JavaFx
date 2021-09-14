0717f56e6c0b73b9f4633eb09109bbc10bcc48b7
andre luis <andreluistma@gmail.com>
2021-09-14 11:05:43 -0300

    melhoria do metodo editar e implentação do date picker no vendedor

--- src/model/dao/impl/SellerDaoJdbc.java
+++ src/model/dao/impl/SellerDaoJdbc.java
@@ -247,7 +247,7 @@ public class SellerDaoJdbc implements SellerDao {
 		obj.setId(rs.getInt("Id"));
 		obj.setName(rs.getString("Name"));
 		obj.setEmail(rs.getString("Email"));
-		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setBirthDate(new java.util.Date(rs.getTimestamp("BirthDate").getTime()));
 		obj.setBaseSalary(rs.getDouble("BaseSalary"));
 		obj.setDepartment(dep);
 
